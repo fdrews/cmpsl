5dc194631cb3af887e71d3226a203669384c21cd
Florian Drews <florian.drews@gecko.de>
2018-03-22 22:00:21 +0100

    grid northSouthWest added

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,17 +1,25 @@
 import * as React from 'react'
 import PropTypes from 'prop-types'
 
+import { merge } from 'lodash'
+
 const baseStyleLayout = {
   display: 'grid',
   height: '100vh',
 }
 
-const northWestSouthGrid = {}
+const northSouthWestGrid = {
+  gridTemplateRows: 'auto 1fr auto',
+  gridTemplateColumns: 'auto 1fr',
+  gridTemplateAreas: '"north north" "west main" "south south"',
+}
 
 export default function Layout({ children }) {
   const numElements = React.Children.forEach(children, (child, key) => {
     console.log('child: ', child)
   })
 
-  return <div style={baseStyleLayout}>replace me!</div>
+  return (
+    <div style={merge(baseStyleLayout, northSouthWestGrid)}>{children}</div>
+  )
 }
--- frontend/pages/hello.tsx
+++ frontend/pages/hello.tsx
@@ -3,6 +3,9 @@ import { Page } from '@seagull/core'
 
 import Layout from '../components/Layout'
 import North from '../components/North'
+import South from '../components/South'
+import West from '../components/West'
+import Main from '../components/Main'
 
 export default class HelloPage extends Page<{}, {}> {
   /**
@@ -17,8 +20,10 @@ export default class HelloPage extends Page<{}, {}> {
     return (
       <div>
         <Layout>
-          <div>BLUBB</div>
           <North />
+          <West />
+          <Main />
+          <South />
         </Layout>
       </div>
     )
